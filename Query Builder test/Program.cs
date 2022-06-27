39b8bd9ad73152a6f882778553720812dbf300e7
eronmiranda <mirandaeronielle@gmail.com>
2022-06-27 16:31:04 -0600

    Created new class for Query Builder

--- Query Builder test/Program.cs
+++ Query Builder test/Program.cs
@@ -1,49 +1,8 @@
 ﻿using System.Reflection;
 using System.Text;
 using static System.Console;
+using Query_Builder_test.Models;
 
-string pathName = GetPath(@"Data/Memberships.txt");
-string entityName = "Memberships";
-string columns = GetColumns(pathName);
-string values = GetValues(pathName);
+Query query = new Query(@"Data/Memberships.txt", "Memberships");
 
-PrintQuery(entityName, columns, values);
-
-// Gets the combined directory name and the file name provided.
-string GetPath(string fileName)
-{
-  string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-  // Removes if its in the development
-  if (directoryName.Contains(@"/bin/Debug/net6.0"))
-  {
-    directoryName = directoryName.Remove((directoryName.Length - (@"/bin/Debug/net6.0").Length));
-  }
-  return Path.Combine(directoryName, fileName);
-}
-
-string GetColumns(string fileName)
-{
-  return File.ReadLines(pathName).First();
-}
-
-string GetValues(string pathName)
-{
-  StringBuilder stringBuilder = new StringBuilder();
-
-  List<string> dataList = File.ReadLines(pathName).ToList();
-  dataList.RemoveAt(0);
-  foreach (string line in dataList)
-  {
-    // /n is added for readability when printed.
-    stringBuilder.Append($"({line}),\n");
-  }
-  // Removes /n and extra comma at the end.
-  stringBuilder.Length -= 2;
-
-  return stringBuilder.ToString();
-}
-
-void PrintQuery(string entityName, string columns, string values)
-{
-  WriteLine($"INSERT INTO {entityName} ({columns}) VALUES \n{values};");
-}
+WriteLine(query.GetQueryString());
