60651c25bc7f5c922388ce8609511de0f1c25e1b
eronmiranda <49223433+eronmiranda@users.noreply.github.com>
2022-06-27 13:51:13 -0600

    Created a test query builder to read from a text file

---
+++ Query Builder test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using static System.Console;
+
+const string MEMBERSHIP_FILE = @"C:\Users\Eron\Documents\Git\BAIST-Capstone-Project\ClubBAIST-GQL\Query Builder test\Memberships.txt";
+
+string entityName, columns;
+entityName = "Memberships";
+columns = "[Description]";
+
+StringBuilder stringBuilder = new StringBuilder();
+
+foreach (string line in File.ReadLines(MEMBERSHIP_FILE))
+{
+  stringBuilder.Append($"({line}),\n");
+}
+stringBuilder.Length -= 2;
+
+WriteLine($"INSERT INTO {entityName} ({columns}) VALUES \n{stringBuilder.ToString()};");
