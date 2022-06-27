70ac324aa59c6203cd8cbf23bac5b844e269eab5
eronmiranda <mirandaeronielle@gmail.com>
2022-06-27 15:52:36 -0600

    Created methods. Converted literal strings to dynamic strings.

--- Query Builder test/Program.cs
+++ Query Builder test/Program.cs
@@ -1,18 +1,49 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
 using static System.Console;
 
-const string MEMBERSHIP_FILE = @"C:\Users\Eron\Documents\Git\BAIST-Capstone-Project\ClubBAIST-GQL\Query Builder test\Memberships.txt";
+string pathName = GetPath(@"Data/Memberships.txt");
+string entityName = "Memberships";
+string columns = GetColumns(pathName);
+string values = GetValues(pathName);
 
-string entityName, columns;
-entityName = "Memberships";
-columns = "[Description]";
+PrintQuery(entityName, columns, values);
 
-StringBuilder stringBuilder = new StringBuilder();
-
-foreach (string line in File.ReadLines(MEMBERSHIP_FILE))
+// Gets the combined directory name and the file name provided.
+string GetPath(string fileName)
 {
-  stringBuilder.Append($"({line}),\n");
+  string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+  // Removes if its in the development
+  if (directoryName.Contains(@"/bin/Debug/net6.0"))
+  {
+    directoryName = directoryName.Remove((directoryName.Length - (@"/bin/Debug/net6.0").Length));
+  }
+  return Path.Combine(directoryName, fileName);
 }
-stringBuilder.Length -= 2;
 
-WriteLine($"INSERT INTO {entityName} ({columns}) VALUES \n{stringBuilder.ToString()};");
+string GetColumns(string fileName)
+{
+  return File.ReadLines(pathName).First();
+}
+
+string GetValues(string pathName)
+{
+  StringBuilder stringBuilder = new StringBuilder();
+
+  List<string> dataList = File.ReadLines(pathName).ToList();
+  dataList.RemoveAt(0);
+  foreach (string line in dataList)
+  {
+    // /n is added for readability when printed.
+    stringBuilder.Append($"({line}),\n");
+  }
+  // Removes /n and extra comma at the end.
+  stringBuilder.Length -= 2;
+
+  return stringBuilder.ToString();
+}
+
+void PrintQuery(string entityName, string columns, string values)
+{
+  WriteLine($"INSERT INTO {entityName} ({columns}) VALUES \n{values};");
+}
