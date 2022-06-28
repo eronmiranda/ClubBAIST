593dc18321d2aa57637cecf5ab277489510bea16
eronmiranda <49223433+eronmiranda@users.noreply.github.com>
2022-06-27 21:19:50 -0600

    Update Insert-Data-Query.sql

--- DB Queries/Insert-Data-Query.sql
+++ DB Queries/Insert-Data-Query.sql
@@ -1,17 +1,11 @@
 USE ClubBAISTDB;
 
 -- Memberships Table
-INSERT INTO Memberships ([Description])
-VALUES ('Gold');
-
-INSERT INTO Memberships ([Description])
-VALUES ('Silver');
-
-INSERT INTO Memberships ([Description])
-VALUES ('Bronze');
-
-INSERT INTO Memberships ([Description])
-VALUES ('Copper');
+INSERT INTO Memberships ([Description]) VALUES
+('Gold'),
+('Silver'),
+('Bronze'),
+('Copper');
 
 -- Members Table
 INSERT INTO Members ([FirstName]
