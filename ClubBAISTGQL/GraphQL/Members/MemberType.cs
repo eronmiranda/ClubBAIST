71bd2262c65e7afad97b630a590ab71265fa320a
eronmiranda <49223433+eronmiranda@users.noreply.github.com>
2022-07-04 22:03:11 -0600

    Resolved issues by including all missing types in the services list on Program.cs file.

--- ClubBAISTGQL/GraphQL/Members/MemberType.cs
+++ ClubBAISTGQL/GraphQL/Members/MemberType.cs
@@ -14,6 +14,12 @@ namespace ClubBAISTGQL.GraphQL.Members
         .ResolveWith<Resolvers>(m => m.GetMembership(default!, default!))
         .UseDbContext<AppDbContext>()
         .Description("Represents the level of membership the member has.");
+
+      descriptor
+        .Field(m => m.MemberTeeTimes)
+        .ResolveWith<Resolvers>(m => m.GetMemberTeeTimes(default!, default!))
+        .UseDbContext<AppDbContext>()
+        .Description("List of Member and Tee Time relationships.");
     }
 
     private class Resolvers
@@ -21,6 +27,11 @@ namespace ClubBAISTGQL.GraphQL.Members
       public Membership GetMembership([Parent] Member member, [ScopedService] AppDbContext context)
       {
         return context.Memberships.FirstOrDefault(m => m.MembershipID == member.MembershipID);
+      }
+
+      public IQueryable<MemberTeeTime> GetMemberTeeTimes([Parent] Member member, [ScopedService] AppDbContext context)
+      {
+        return context.MemberTeeTimes.Where(m => m.MemberNumber == member.MemberNumber);
       }
     }
   }
